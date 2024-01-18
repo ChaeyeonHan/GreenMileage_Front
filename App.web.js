4dd4b86efbcecf46e3914946c57860994eaf0b68
ChaeyeonHan <kate2236e@naver.com>
2024-01-18 20:20:58 +0900

    Initial commit

---
+++ App.web.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native-web'
+
+function App() {
+    return (
+        <View>
+            <Text> 드디어 완성 </Text>
+        </View>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const appDirectory = path.resolve(__dirname);
+
+const babelLoaderConfiguration = {
+    test: /\.js$/,
+    use: {
+        loader: 'babel-loader',
+        options: {
+            cacheDirectory: true,
+            presets: ['@babel/preset-react'],
+            plugins: ['react-native-web']
+        }
+    }
+};
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        path.resolve(appDirectory, 'index.web.js')
+    ],
+    output: {
+        filename: 'bundle.web.js',
+        path: path.resolve(appDirectory, 'dist')
+    },
+
+    module: {
+        rules: [
+        babelLoaderConfiguration,
+        ]
+    },
+
+    // plugins:[new HtmlWebpackPlugin({ template: './public/index.html'})],
+    plugins: [
+        new HtmlWebpackPlugin({template: path.resolve(appDirectory, 'public', 'index.html')})
+    ],
+
+    resolve: {
+        alias: {
+        'react-native$': 'react-native-web'
+        },
+        extensions: [ '.web.js', '.js' ]
+    }
+}
