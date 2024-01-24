7de7a381e9a6f9bc9c10621979d7336e3bca71b1
ChaeyeonHan <kate2236e@naver.com>
2024-01-24 11:50:02 +0900

    feat: points

--- campaign.js
+++ campaign.js
@@ -7,31 +7,60 @@ function Campaign() {
     const [participationStatus, setParticipationStatus] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch('http://localhost:3001/campaign/info');
-            const data = await response.json();
-            setCampaign(data);
+      // 토큰 가져오기
+      const storedToken = localStorage.getItem('authToken');
+      if (!storedToken) {
+        console.log("토큰없어여");
+          // 토큰이 없으면 로그인 페이지로 리다이렉트
+          return;
+      }
 
-            // 캠페인별 참여상태 초기화
-            const initialStatus = data.reduce((acc, campaign) => {
-              acc[campaign.title] = false;
-              return acc;
-            }, {});
-              setParticipationStatus(initialStatus);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-    
-        fetchData();
+      const fetchData = async () => {
+        try {
+          const response = await fetch('http://localhost:3001/campaign/info');
+          const data = await response.json();
+
+          // 각 캠페인에 랜덤 포인트 추가
+          const dataPoints = data.map(item => ({
+            ...item,
+            points: 1+ Math.floor(Math.random() * 5)
+          }));
+          setCampaign(dataPoints);
+
+          // 캠페인별 참여상태 초기화
+          const initialStatus = data.reduce((acc, campaign) => {
+            acc[campaign.title] = false;
+            return acc;
+          }, {});
+            setParticipationStatus(initialStatus);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+      fetchData();
       }, []); // 빈 배열은 컴포넌트가 처음 마운트될 때 한 번만 실행
 
-    const handleParticipation = (title) => {
-    setParticipationStatus(prevStatus => ({
-        ...prevStatus,
-        [title]: !prevStatus[title] // 특정 캠페인의 참여 상태만 토글
-    }));
+    const handleParticipation = async (title) => {
+      try {
+        setParticipationStatus(prevStatus => ({
+            ...prevStatus,
+            [title]: !prevStatus[title] // 특정 캠페인의 참여 상태만 토글
+        }));
+
+        const response = await fetch('http://localhost"3000/users/campaign', {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ${storedToken}'
+          },
+          body: JSON.stringify({
+            addPoints: selectedCampaign.points
+          })
+        });
+      } catch (error) {
+        console.error('Error during campaign participation:', error);
+      }
   };
 
 
@@ -54,9 +83,12 @@ function Campaign() {
             {campaigns.map((campaign) => (
                 <div key={campaign.id} className="card">
                     <h2>{campaign.title}</h2>
-                    <img src={campaign.imageUrl} alt={campaign.title} className="card-image" />
+                    <img src={campaign.image} alt={campaign.title} className="card-image" />
+                    <div className='campaign-info-container'>
+                      <a href={campaign.link} target="_blank" rel="noopener noreferrer" className="campaign-read-more">Read More</a>
+                      <p className='campaign-points'>{campaign.points} point</p>
+                    </div>
                     {/* 기타 캠페인 정보 */}
-                    <a href={campaign.link} target="_blank" rel="noopener noreferrer">Read More</a>
                     <div className="button-container" style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px' }}>
                         <button className="button" onClick={() => openModal(campaign)}>
                             {campaign.participants}명 참여중
